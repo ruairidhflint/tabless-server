6772fcb464eb7bb29088373447d51cf83d572198
Rory Flint <rory@roryflint.co.uk>
2019-10-04 08:28:28 +0100

    updated tabs table created

---
+++ database/migrations/20191004082332_createTabsTable.js
@@ -0,0 +1,26 @@
+
+exports.up = function (knex) {
+    return knex.schema
+      .createTable('tabs', (table) => {
+        table.increments();
+        table.string('url')
+          .notNullable();
+        table.string('title')
+          .notNullable();
+        table.blob('description');
+        table.string('image');
+        table.integer('user_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('users')
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE');
+      });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema
+      .dropTableIfExists('tabs');
+  };
+  
